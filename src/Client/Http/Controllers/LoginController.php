7050d624f0584f2dc2f2b48b5642b9100e90f05b
Sebastian Hübner <sebastian@okolowski.de>
2018-07-11 12:40:13 +0200

    Signout Template

--- src/Client/Http/Controllers/LoginController.php
+++ src/Client/Http/Controllers/LoginController.php
@@ -9,19 +9,19 @@ class LoginController extends BaseController
 
     public function login()
     {
-        $server_url = config('jwt.auth_server') . '/client/login?redirect_uri=' . config('app.url');
+        $server_url = config('jwt.auth_server') . '/login?redirect_uri=' . config('app.url');
         return redirect()->to($server_url);
     }
 
     public function logout()
     {
-        $server_url = config('jwt.auth_server') . '/client/logout?continue=' . route('signout');
+        $server_url = config('jwt.auth_server') . '/logout?continue=' . route('signout');
         return redirect()->to($server_url);
     }
 
     public function password()
     {
-        $server_url = config('jwt.auth_server') . '/client/password?redirect_uri=' . config('app.url');
+        $server_url = config('jwt.auth_server') . '/password?redirect_uri=' . config('app.url');
         return redirect()->to($server_url);
     }
 
